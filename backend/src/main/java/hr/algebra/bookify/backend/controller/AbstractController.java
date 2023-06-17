087f6966618457dfc6c4b6deae70441fd1018e8e
Raphystolage <raphael.morales31@gmail.com>
2023-06-17 21:32:53 +0200

    Add ActiveMQ

--- backend/src/main/java/hr/algebra/bookify/backend/controller/AbstractController.java
+++ backend/src/main/java/hr/algebra/bookify/backend/controller/AbstractController.java
@@ -1,7 +1,9 @@
 package hr.algebra.bookify.backend.controller;
 
 import hr.algebra.bookify.backend.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,34 +15,41 @@ public abstract class AbstractController<T> {
 
     protected AbstractService<T> service;
 
+    @Autowired
+    protected JmsTemplate JMS_TEMPLATE;
+
     public AbstractController(AbstractService<T> service) {
         this.service = service;
     }
 
-    @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("{id}")
     public T getById(@PathVariable final Long id) {
+        JMS_TEMPLATE.convertAndSend("Get objects by id " + id);
         return service.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping
     public List<T> getAll() {
+        JMS_TEMPLATE.convertAndSend("Get all objects");
         return service.getAll();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public T create(@RequestBody T newStorable) {
-        return service.create(newStorable);
+    public T create(@RequestBody T newObject) {
+        JMS_TEMPLATE.convertAndSend("Create object");
+        return service.create(newObject);
     }
 
     @PutMapping
     public T update(@RequestBody T updatedClassicDBStorable) {
+        JMS_TEMPLATE.convertAndSend("Update object");
         return service.update(updatedClassicDBStorable);
     }
 
     @DeleteMapping("{id}")
     public void deleteById(@PathVariable Long id) {
+        JMS_TEMPLATE.convertAndSend("Delete object by id " + id);
         service.deleteById(id);
     }
 
